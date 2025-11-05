cc5ca5e457845050161df866610c52618e4444e6
Eric Buehler <ericlbuehler@gmail.com>
2025-11-05 18:34:02 -0500

    Update ug dep

--- candle-core/benches/benchmarks/mod.rs
+++ candle-core/benches/benchmarks/mod.rs
@@ -25,8 +25,7 @@ impl BenchDevice for Device {
             Device::Cuda(device) => {
                 #[cfg(feature = "cuda")]
                 {
-                    use cuda::WrapErr;
-                    return Ok(device.synchronize().w()?);
+                    return Ok(device.synchronize()?);
                 }
                 #[cfg(not(feature = "cuda"))]
                 panic!("Cuda device without cuda feature enabled: {device:?}")
